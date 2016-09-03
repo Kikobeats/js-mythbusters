258ed7949bbfe73499cb0222602e367f93681a6a
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-09-03 17:09:32 +0200

    Add changelog integration

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,9 +10,25 @@ function ready (fn) {
   }
 }
 
-ready(function () {
-  var links = document.querySelectorAll('.markdown-body a')
-  links.forEach(function (link) {
+function addChangelog (el) {
+  window.HW_config = {
+    selector: '.link-index',
+    account: 'xGn9Ay'
+  }
+
+  var script = document.createElement('script')
+  script.async = true
+  script.src = 'https://cdn.headwayapp.co/widget.js'
+  el.appendChild(script)
+}
+
+function addExternalLinks (elements) {
+  elements.forEach(function (link) {
     link.setAttribute('target', '_blank')
   })
+}
+
+ready(function () {
+  addChangelog(document.querySelector('head'))
+  addExternalLinks(document.querySelectorAll('.markdown-body a'))
 })
